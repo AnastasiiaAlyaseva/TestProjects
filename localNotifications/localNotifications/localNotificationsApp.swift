8f29e35926f84afb8d3ba52065437f0503344488
Anastasiia Alyaseva <180915an@gmail.com>
2023-07-13 15:20:13 +0200

    added localNotification project

---
+++ localNotifications/localNotifications/localNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  localNotificationsApp.swift
+//  localNotifications
+//
+//  Created by Anastasiia Alyaseva on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct localNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
