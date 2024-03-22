b5d31576a1015e0f75a0a10640ca370dd43ce10c
Anastasiia Alyaseva <180915an@gmail.com>
2024-03-22 16:54:59 +0100

    OwnModifier

---
+++ OwnModifier/OwnModifier/OwnModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnModifierApp.swift
+//  OwnModifier
+//
+//  Created by Anastasiia Alyaseva on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OwnModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
