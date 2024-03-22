6ea3fcbe1402202099a7d0c1af5d7436e6a3604e
Anastasiia Alyaseva <180915an@gmail.com>
2024-03-22 17:16:56 +0100

    CustomContainers

---
+++ CustomContainers/CustomContainers/CustomContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContainersApp.swift
+//  CustomContainers
+//
+//  Created by Anastasiia Alyaseva on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
