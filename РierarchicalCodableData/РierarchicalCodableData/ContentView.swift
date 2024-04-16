a995431c16f916882cc573379d130783f8ce38c0
Anastasiia Alyaseva <180915an@gmail.com>
2024-04-16 16:24:39 +0200

    Working with hierarchical Codable data

---
+++ РierarchicalCodableData/РierarchicalCodableData/ContentView.swift
@@ -0,0 +1,37 @@
+
+import SwiftUI
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+        {
+            "name": "Anastasiia",
+            "address": {
+                "street": "12345, Berlin Avenue",
+                "city": "Berlin"
+            }
+        }
+"""
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ РierarchicalCodableData/РierarchicalCodableData/_ierarchicalCodableDataApp.swift
@@ -0,0 +1,12 @@
+
+
+import SwiftUI
+
+@main
+struct _ierarchicalCodableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
