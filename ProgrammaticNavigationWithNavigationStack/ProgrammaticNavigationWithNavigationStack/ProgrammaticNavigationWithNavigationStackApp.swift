af6cd0013df1b05616347ccadb18b188cab3927f
Anastasiia Alyaseva <180915an@gmail.com>
2024-06-25 16:55:47 +0200

    example

---
+++ ProgrammaticNavigationWithNavigationStack/ProgrammaticNavigationWithNavigationStack/ProgrammaticNavigationWithNavigationStackApp.swift
@@ -0,0 +1,12 @@
+
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavigationWithNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
