a14efb7bc7aec28a70a8410d1939eda6b8e123ea
Anastasiia Alyaseva <180915an@gmail.com>
2024-07-01 12:35:36 +0200

    example

---
+++ EditableNavigationTitle/EditableNavigationTitle/ContentView.swift
@@ -0,0 +1,19 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var title = "SwiftUI"
+    
+    var body: some View {
+        NavigationStack{
+            Text("Hello,world!")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
