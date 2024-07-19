2041bbc099cc4b990385b1b02a6970748466f035
Anastasiia Alyaseva <180915an@gmail.com>
2024-07-19 13:06:06 +0200

    Sending and receiving Codable data with URLSession

---
+++ SendingReceivingCodableDataWithURLSession/SendingReceivingCodableDataWithURLSession/SendingReceivingCodableDataWithURLSessionApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct SendingReceivingCodableDataWithURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
