5df3893e0a5e905ab872f2b32c204387a6c825c3
Anastasiia Alyaseva <180915an@gmail.com>
2024-08-07 17:27:33 +0200

    Integrating Core Image with SwiftUI

---
+++ IntegratingCoreImage/IntegratingCoreImage/ContentView.swift
@@ -0,0 +1,48 @@
+
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var image: Image?
+    
+    var body: some View {
+        VStack {
+           image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .photo)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+        
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
