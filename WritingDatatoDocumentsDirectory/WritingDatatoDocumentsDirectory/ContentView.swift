7e10ba7e081fc8d5ead282223023d7fe39ee53a9
Anastasiia Alyaseva <180915an@gmail.com>
2024-08-16 15:17:24 +0200

    Writing data to the documents directory

---
+++ WritingDatatoDocumentsDirectory/WritingDatatoDocumentsDirectory/ContentView.swift
@@ -0,0 +1,25 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Button("Read and Write") {
+            let data = Data("Test Message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
