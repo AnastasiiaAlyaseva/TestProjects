b46da86cc428f1280e58fc8f6924ee69e476d9e5
Anastasiia Alyaseva <180915an@gmail.com>
2024-09-10 15:27:21 +0200

    Custom tow swipe actions to a List

---
+++ CustomRowSwipeActions/CustomRowSwipeActions/ContentView.swift
@@ -0,0 +1,26 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Anastasiia Smirnova")
+                .swipeActions {
+                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
+                        print("Deleting")
+                    }
+                }
+                .swipeActions(edge: .leading) {
+                    Button("Pin", systemImage: "pin") {
+                        print("Pinning")
+                    }
+                    .tint(.orange)
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
