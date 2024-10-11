71004b8996a449fefa0be2042dcd954abc37b8c5
Anastasiia Alyaseva <180915an@gmail.com>
2024-10-11 16:31:31 +0200

    gesture sequences where one gesture will only become active if another gesture has first succeeded

---
+++ GestureSequences/GestureSequences/ContentView.swift
@@ -0,0 +1,44 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+    
+    var body: some View {
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+            }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.blue)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+#Preview {
+    ContentView()
+}
